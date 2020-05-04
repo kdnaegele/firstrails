09a8739a4152c6d5c99af10d81067ee293f7590d
Kevin Naegele <kdnaegele@gmail.com>
2020-05-03 23:14:01 -0700

    chaning to tweets from twit

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users do
-    resoutces :twits
+  resources :twits
   end
 
-  
+
 end
